920f5940e8733f8ff7e04f75af091bbb884da164
FIRST_NAME LAST_NAME <example@example.com>
2023-05-10 22:40:52 -0500

    lab 13

---
+++ client/src/components/AboutSite.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2> Where have you traveled to ?</h2>
+
+    <p>use this site to keep track of which states you have travelled to </p>
+    <p> A vue.js site by spillah </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AboutSite'
+}
+</script>
+
+<styles>
+
+</styles>
---
+++ client/src/components/PageFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+    <div id="navigation-links">
+
+
+      <span class="nav-link">
+      <router-link to="/">Home</router-link>
+      </span>
+
+      <span class="nav-link">
+
+    <router-link to="about">About this site</router-link>
+      </span>
+
+    <p>Header image by
+      <a href="https://unsplash.com/photos/KpBXAOs80YI">@chrislawton on Unsplash</a>
+    </p>
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageFooter'
+}
+</script>
+
+<style scoped>
+.nav-link {
+  margin: 1rem;
+}
+</style>
